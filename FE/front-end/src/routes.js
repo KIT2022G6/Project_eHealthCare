f063f3a5859487e60268369c015fad8140289bcb
ngoducdo <ngo2682@gmail.com>
2022-08-16 10:07:18 +0700

    Drugstores

--- FE/front-end/src/routes.js
+++ FE/front-end/src/routes.js
@@ -15,71 +15,30 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Dashboard from "views/Dashboard.js";
-import UserProfile from "views/UserProfile.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
-import Notifications from "views/Notifications.js";
-import Upgrade from "views/Upgrade.js";
+import Medicine from "views/Drugstores/Medicine";
+import DrugstoresProfile from "views/Drugstores/DrugstoresProfile";
+import MedicalRecord from "views/Drugstores/MedicalRecord";
 
 const dashboardRoutes = [
   {
-    upgrade: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-alien-33",
-    component: Upgrade,
-    layout: "/admin"
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "nc-icon nc-chart-pie-35",
-    component: Dashboard,
-    layout: "/admin"
-  },
-  {
-    path: "/user",
-    name: "User Profile",
+    path: "/Drugstores/DrugstoresProfile",
+    name: "Drugstores Profile",
     icon: "nc-icon nc-circle-09",
-    component: UserProfile,
+    component: DrugstoresProfile,
     layout: "/admin"
   },
   {
-    path: "/table",
-    name: "Table List",
+    path: "/Drugstores/Medicine",
+    name: "Medicine",
     icon: "nc-icon nc-notes",
-    component: TableList,
+    component: Medicine,
     layout: "/admin"
   },
   {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-paper-2",
-    component: Typography,
-    layout: "/admin"
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "nc-icon nc-pin-3",
-    component: Maps,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "nc-icon nc-bell-55",
-    component: Notifications,
+    path: "/Drugstores/MedicalRecord",
+    name: "Medical Record",
+    icon: "nc-icon nc-notes",
+    component: MedicalRecord,
     layout: "/admin"
   }
 ];

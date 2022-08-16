01696f74021baf33526ef32d4d7a0ebc910e5ef9
ngoducdo <108042350+ngoducdo@users.noreply.github.com>
2022-08-16 12:25:58 +0700

    Update routes.js

--- FE/front-end/src/routes.js
+++ FE/front-end/src/routes.js
@@ -1,69 +1,76 @@
+/*!
 
-import Dashboard from "views/Dashboard.js";
-import UserProfile from "views/UserProfile.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
-import Notifications from "views/Notifications.js";
-import Upgrade from "views/Upgrade.js";
-import BaoHiem from "views/BaoHiem.js";
+=========================================================
+* Light Bootstrap Dashboard React - v2.0.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Medicine from "views/Drugstores/Medicine";
+import DrugstoresProfile from "views/Drugstores/DrugstoresProfile";
+import MedicalRecord from "views/Drugstores/MedicalRecord";
+import DoctorProfile from "views/Doctor/DoctorProfile";
+import MedicalRecord1 from "views/Doctor/MedicalRecord";
+import Schedule from "views/Doctor/Schedule";
+import MedicalExaminationList from "views/Doctor/MedicalExaminationList";
 
 const dashboardRoutes = [
   {
-    path: "/dashboard",
-    name: "Tài khoản",
-    icon: "nc-icon nc-chart-pie-35",
-    component: Dashboard,
-    layout: "/admin"
-  },
-  {
-    path: "/user",
-    name: "User Profile",
+    path: "/Drugstores/DrugstoresProfile",
+    name: "Drugstores Profile",
     icon: "nc-icon nc-circle-09",
-    component: UserProfile,
+    component: DrugstoresProfile,
     layout: "/admin"
   },
   {
-    path: "/table",
-    name: "Table List",
+    path: "/Drugstores/Medicine",
+    name: "Medicine",
     icon: "nc-icon nc-notes",
-    component: TableList,
+    component: Medicine,
     layout: "/admin"
   },
   {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-paper-2",
-    component: Typography,
+    path: "/Drugstores/MedicalRecord",
+    name: "Medical Record",
+    icon: "nc-icon nc-notes",
+    component: MedicalRecord,
     layout: "/admin"
   },
   {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
+    path: "/Doctor/DoctorProfile",
+    name: "Doctor Profile",
+    icon: "nc-icon nc-circle-09",
+    component: DoctorProfile,
     layout: "/admin"
   },
   {
-    path: "/maps",
-    name: "Maps",
-    icon: "nc-icon nc-pin-3",
-    component: Maps,
+    path: "/Doctor/MedicalRecord",
+    name: "Medical Record",
+    icon: "nc-icon nc-notes",
+    component: MedicalRecord1,
     layout: "/admin"
   },
   {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "nc-icon nc-bell-55",
-    component: Notifications,
+    path: "/Doctor/Schedule",
+    name: "Schedule",
+    icon: "nc-icon nc-notes",
+    component: Schedule,
     layout: "/admin"
   },
   {
-    path: "/insurances",
-    name: "Insurances",
-    icon: "nc-icon nc-bell-55",
-    component: BaoHiem,
+    path: "/Doctor/MedicalExaminationList",
+    name: "Medical Examination List",
+    icon: "nc-icon nc-notes",
+    component: MedicalExaminationList,
     layout: "/admin"
   }
 ];
